a3262f9fc71699699bb2727d822a07c15cdc7db0
ohr5446 <65328774+ohr5446@users.noreply.github.com>
2020-10-13 18:59:21 +0900

    미추홀 오르세 DTO

---
+++ dto/MemberDTO.java
@@ -0,0 +1,53 @@
+package dto;
+
+public class MemberDTO {
+	private String id;
+	private String pw;
+	private String name;
+	private String fArtist;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getfArtist() {
+		return fArtist;
+	}
+	public void setfArtist(String fArtist) {
+		this.fArtist = fArtist;
+	}
+	
+	
+	public MemberDTO() {
+		super();
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "MemberDTO [id=" + id + ", pw=" + pw + ", name=" + name + ", fArtist=" + fArtist + "]";
+	}
+	
+	
+
+	
+	
+	
+
+	
+	
+	
+}
